fabc25a6698e62e909748bdaf9fc85e742ba7849
Gozzah <rune.halvor@yahoo.dk>
2018-05-28 19:28:11 +0200

    Add files via upload

---
+++ period_Four/GraphQL_Essentials/apollo_client/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { render } from "react-dom";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+const ExchangeRates = () => (
+  <Query
+    query={gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error :(</p>;
+
+      return data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+          <p>{`${currency}: ${rate}`}</p>
+        </div>
+      ));
+    }}
+  </Query>
+);
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <ExchangeRates/>
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById("root"))
+
+export default App;
+
+
+
